559649aad00528d5cf139066db5338dd72d28656
chetheaker <che.theaker@gmail.com>
2022-12-20 17:54:52 +0000

    express added

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,12 @@
-console.log("hello world")
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 4040;
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+})
