f758cd16ef1ab951355d3b1d9265c10e5a391685
chetheaker <che.theaker@gmail.com>
2022-12-20 18:54:52 +0000

    graphql added'

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,68 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-import express from 'express';
-import router from './routers/router';
+// import express from 'express';
+// import router from './routers/router';
 
-const app = express();
+// const app = express();
 
-const PORT = process.env.PORT || 4040;
+// const PORT = process.env.PORT || 4040;
 
-app.use(router);
+// app.use(router);
 
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-})
+// app.listen(PORT, () => {
+//   console.log(`Server listening at http://localhost:${PORT}`);
+// })
+
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `
+  type Book {
+    title: String
+    author: String
+    age: Int
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+    age: 13
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+    age: 66
+  },
+];
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
