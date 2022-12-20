b58d05ff9ea4934dba2b66322d5bfbe1821983c6
chetheaker <che.theaker@gmail.com>
2022-12-20 18:06:04 +0000

    router added

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,14 @@
-import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+
+import express from 'express';
+import router from './routers/router';
 
 const app = express();
 
 const PORT = process.env.PORT || 4040;
+
+app.use(router);
 
 
 app.listen(PORT, () => {
