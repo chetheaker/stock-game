5e126a0dc72b81d48726a79b987c5b365cade03b
chetheaker <che.theaker@gmail.com>
2022-12-21 12:43:44 +0000

    graphql mutation user added

--- server/graphql/resolvers.ts
+++ server/graphql/resolvers.ts
@@ -1,12 +1,31 @@
+import prisma from '../prisma/prisma.js';
+import { User } from '../types/auth.js';
+import { GraphQLError } from 'graphql';
+
 export const resolvers = {
   Query: {
-    user(_, args) {
-      return {
-        id: args.id,
-        email: 'email',
-        password: 'passs',
-        username: 'username'
-      };
+    async user(_, args) {
+      const user = await prisma.user.findFirst({
+        where: { id: args.id }
+      });
+      console.log(user);
+      if (user) {
+        return user;
+      } else {
+        throw new GraphQLError('user with this id does not exist');
+      }
+    }
+  },
+  Mutation: {
+    async createUser(_, args: User) {
+      const isExist = await prisma.user.findFirst({
+        where: { email: args.email }
+      });
+      if (isExist === null) {
+        return await prisma.user.create({ data: { ...args } });
+      } else {
+        throw new GraphQLError('user with this email already exists');
+      }
     }
   }
 };
